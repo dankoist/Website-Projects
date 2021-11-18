e052a6ed888defc12b0e61fe5749698935e19406
Cody Shitagi <91982426+dankoist@users.noreply.github.com>
2021-11-18 21:38:41 +0000

    0.000 (Converting webm to mp3 files, better than cloud convert)

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hi')
+from pydub import AudioSegment
+
+wav_audio = AudioSegment.from_file("MountainKing.webm", format="webm")
+raw_audio = AudioSegment.from_file("MountainKing.webm", format="raw", frame_rate=44100, channels=2, sample_width=2)           
+
+file_handle = wav_audio.export("output.mp3", format="mp3")
